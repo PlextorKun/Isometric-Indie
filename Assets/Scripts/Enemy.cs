fcc56d93f5ee05f2c5fa936031059f492a30e83d
Sambodh Mitra <sambodh.mitra@berkeley.edu>
2020-01-29 21:42:06 -0800

    Added enemy, enemy movement, and enemy animation.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    #region movement_variables
+    public float movespeed;
+    #endregion
+
+    #region physics_components
+    Rigidbody2D enemyRB;
+    #endregion
+
+    #region targeting_variables
+    public Transform player;
+    #endregion
+
+    #region attack_variables
+    public float explosionDamage;
+    public float explosionRadius;
+    public GameObject explosionObj;
+
+    #endregion
+
+    #region Unity_functions
+    private void Awake()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (player == null)
+        {
+            return;
+        }
+
+        Move();
+    }
+    #endregion
+
+    #region movement_functions
+
+    private void Move()
+    {
+        Vector2 direction = player.position - transform.position;
+        enemyRB.velocity = direction.normalized * movespeed;
+    }
+    #endregion
+
+    #region attack_functions
+
+    private void Explode()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, explosionRadius, Vector2.zero);
+        foreach(RaycastHit2D hit in hits)
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                Debug.Log("Hit Player with explosion");
+
+                Instantiate(explosionObj, transform.position, transform.rotation);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Explode();
+        }
+    }
+    #endregion
+}
