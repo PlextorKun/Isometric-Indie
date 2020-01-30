ede6c10f0e12c73fe9f282fe0865fc39be2a0274
Sambodh Mitra <sambodh.mitra@berkeley.edu>
2020-01-29 16:33:14 -0800

    Updated player movement and added animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,22 +10,52 @@ public class PlayerController : MonoBehaviour
     float y_input;
     #endregion
 
+    #region attack_variables
+    public float damage;
+    public float attackspeed;
+    float attackTimer;
+    public float hitboxTiming;
+    public float endAnimationTiming;
+    bool isAttacking;
+    Vector2 currDirection;
+    #endregion
+
     #region physics_components
     Rigidbody2D playerRB;
+    #endregion
+
+    #region animation_components
+    Animator anim;
     #endregion
 
     #region Unity_functions
     private void Awake()
     {
         playerRB = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
+
+        attackTimer = 0;
     }
 
     private void Update()
     {
+        if (isAttacking)
+        {
+            return;
+        }
         x_input = Input.GetAxisRaw("Horizontal");
         y_input = Input.GetAxisRaw("Vertical");
 
         Move();
+
+        if (Input.GetKeyDown(KeyCode.J) && attackTimer <= 0)
+        {
+            Attack();
+        } else
+        {
+            attackTimer -= Time.deltaTime;
+        }
     }
     #endregion
 
@@ -33,27 +63,75 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
+        anim.SetBool("Moving", true);
+
         if (x_input > 0)
         {
             playerRB.velocity = Vector2.right * movespeed;
+            currDirection = Vector2.right;
         } 
         else if (x_input < 0)
         {
             playerRB.velocity = Vector2.left * movespeed;
+            currDirection = Vector2.left;
         }
         else if (y_input > 0)
         {
             playerRB.velocity = Vector2.up * movespeed;
+            currDirection = Vector2.up;
         }
         else if (y_input < 0)
         {
             playerRB.velocity = Vector2.down * movespeed;
+            currDirection = Vector2.down;
         }
         else
         {
             playerRB.velocity = Vector2.zero;
+            anim.SetBool("Moving", false);
         }
 
+        anim.SetFloat("DirX", currDirection.x);
+        anim.SetFloat("DirY", currDirection.y);
+    }
+    #endregion
+
+    #region attack_functions
+    private void Attack()
+    {
+        Debug.Log("Attacking now!");
+        Debug.Log(currDirection);
+
+        StartCoroutine(AttackRoutine());
+
+        attackTimer = attackspeed;
+    }
+
+    IEnumerator AttackRoutine()
+    {
+
+        isAttacking = true;
+        playerRB.velocity = Vector2.zero;
+
+        anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(hitboxTiming);
+
+        Debug.Log("Cast hitbox now.");
+
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(playerRB.position + currDirection, Vector2.one, 0f, Vector2.zero, 0);
+        foreach (RaycastHit2D hit in hits)
+        {
+            Debug.Log(hit.transform.name);
+            if (hit.transform.CompareTag("Enemy"))
+            {
+                Debug.Log("tons of damage");
+            }
+        }
+
+        yield return new WaitForSeconds(endAnimationTiming);
+
+        isAttacking = false;
     }
     #endregion
 }
